46a56a2cec474be798780722c64c51aa1e285248
ShatVM <pzs_shvm@ztu.edu.ua>
2025-02-18 12:24:49 +0200

    Add initial implementation of Valentine's Day webpage with styling and interactive wishes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('btn_love_wishes').addEventListener('click', () => {    
+   document.getElementById('p-love-wishes').innerText = "Нехай кожен день приносить радість і тепло!" 
+})
+
+
+
+
+
